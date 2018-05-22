0864d0e6bf7c08d8575ded7f9547ed20dc6561d4
Abel <Abel.Chen@feisu.com>
2018-05-22 18:57:41 +0800

    finish

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function initialize () {
     mainWindow = new BrowserWindow(windowOptions)
     mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
     // Launch fullscreen with DevTools open, usage: npm run debug
-    if (true) {
+    if (debug) {
       mainWindow.webContents.openDevTools()
       mainWindow.maximize()
       require('devtron').install()
